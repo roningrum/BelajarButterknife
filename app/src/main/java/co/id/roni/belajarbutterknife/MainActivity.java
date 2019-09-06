587b3098f0d21aa523c64279582f39152f2f738e
Rizki Oktavia Ningrum <rizkioktavianingrum@gmail.com>
2019-09-06 11:04:30 +0700

    Initial commit

---
+++ app/src/main/java/co/id/roni/belajarbutterknife/MainActivity.java
@@ -0,0 +1,26 @@
+package co.id.roni.belajarbutterknife;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.BindString;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.tv_belajar_butteknife)
+    TextView tvBelajarBK;
+    @BindString(R.string.salam_butterknife)
+    String helloButterKnife;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        tvBelajarBK.setText(helloButterKnife);
+    }
+}
